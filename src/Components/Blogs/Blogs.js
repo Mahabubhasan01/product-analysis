101e1279a1e90d7bdb30c9683876199f623a324b
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-04 21:47:48 +0600

    Question and answer done

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -6,16 +6,19 @@ const Blogs = () => {
         <div>
             <div className='blogs'>
                 <div className='blog'>
-                    <h3>What is Context Api</h3><br />
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit consectetur praesentium, neque, sequi eveniet magni beatae accusamus maxime deleniti excepturi adipisci doloremque libero, ex tempora tenetur minus illo quisquam reiciendis molestias consequuntur. Soluta, dignissimos aut! Porro consectetur magnam dolores odit.</p>
+                    <h3>What is Context Api?...</h3><br />
+                    <p>The Context shares information on any component without drilling. We know that react props work synchronously and step by step. Although when we pass data need permission into every other child component. On the other hand, we face a little bit of trouble with this troubling problem solve the context you do not need any trouble feel free to send any data using context that is call or method context API</p>
                 </div>
                 <div className='blog'>
                     <h3>What is Semantic tag</h3><br />
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit consectetur praesentium, neque, sequi eveniet magni beatae accusamus maxime deleniti excepturi adipisci doloremque libero, ex tempora tenetur minus illo quisquam reiciendis molestias consequuntur. Soluta, dignissimos aut! Porro consectetur magnam dolores odit.</p>
+                    <p>Semantic is behind the Html part this is creating a smooth Html component. If you need good UI opposite way you need to style or part your code of the component. Commonly use their kind of part into the semantic like 'header, nav, section, footer'.This is very impacting on every coder.</p>
                 </div>
                 <div className='blog'>
                  <h3>What is the different between inline-element <br />and inline-block element</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit consectetur praesentium, neque, sequi eveniet magni beatae accusamus maxime deleniti excepturi adipisci doloremque libero, ex tempora tenetur minus illo quisquam reiciendis molestias consequuntur. Soluta, dignissimos aut! Porro consectetur magnam dolores odit.</p>
+                    <p>Those are the Html tag which is inline and inline-block.
+                        initially inline don't use width and height. It shows Commonly line by line as far as having space for every element. On the other hand, inline-block varies width and height like blocks way.
+                        Mostly you can identify inline as showing line by line or you can say row style opposite inline-block showing the data column by column. 
+                    </p>
                 </div>
             </div>
         </div>
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { LineChart } from 'recharts';
-import './Dashboard.css'
 
 const Dashboard = () => {
     return (
         <div>
-            <div>
-                <LineChart></LineChart>
-            </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
             
         </div>
     );
